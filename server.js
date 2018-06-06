02482ab5ca2c19d99294686c9407c16d768c66df
TheTruth219 <36748331+TheTruth219@users.noreply.github.com>
2018-06-05 22:12:21 -0700

    Updated Auth0

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+    var express = require('express');
     var bodyParser = require('body-parser');
     var Pusher = require('pusher');
 
@@ -28,7 +28,7 @@ var express = require('express');
 
     app.use(express.static(__dirname + '/public'));
 
-    var port = process.env.PORT || 5000;
+    var port = process.env.PORT || 3000;
     app.listen(port, function () {
       console.log(`app listening on port ${port}!`)
     });
